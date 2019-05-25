d84f8003c737d7787c2bf9711e1e0a318e12bff4
Andrey <lee1975uz@gmail.com>
2019-05-25 18:58:57 -0500

    В ДЗ1 исправлен недочет. ДЗ2 представлен на рассмотрение.

--- js/check-login.js
+++ js/check-login.js
@@ -1,5 +1,88 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
-	console.log('Hello world!');	
+	var myModule = (function(){
+		// Переменные модуля
+		var _form = $('#form'),
+			_yourEmail = $('input[type=email]'),
+			_yourPassword = $('input:password');
+			
+			
+		// Метод инициализации (запуска) модуля
+		var init = function(){
+			_setUpListeners(); // Запускаем прослушку событий
+		};
+
+		
+
+		//Метод прослушки событий
+		//В нем прослушиваем события на странице, например клики по кнопкам, и при срабатывании события запускаем нужный метод нашего модуля
+		var _setUpListeners = function(){
+			_form.on('submit', function(event){
+				_formValidation(event);				
+			});
+			
+		}
+
+		// Приватные методы
+    
+		var _formValidation = function (event) {
+    		event.preventDefault();
+			console.log('Hello from _formValidation()');
+			var emailVal = _yourEmail.val().toLowerCase().trim(),
+			 	passwordVal = _yourPassword.val().trim();
+			 	
+			//Проверка пустые поля
+			 if (emailVal.length == 0){
+			 	$('#emailError').fadeIn(1000);
+			 	event.preventDefault();
+			 } else {
+			 	$('#emailError').fadeOut(1000);
+			 };
+
+			 if (passwordVal.length == 0){
+			 	$('#passwordError').fadeIn(1000);
+			 	event.preventDefault();
+			 } else {
+			 	$('#passwordError').fadeOut(1000);			 	
+			 }
+
+			 //Проверка на правильный формат email
+			 if ( emailVal !== '' ) {			 	
+				var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+				if ( pattern.test( emailVal ) ) {
+					$('#wrongEmailFormat').fadeOut(1000);
+					console.log('Email is VALID');
+				} else {
+					$('#wrongEmailFormat').fadeIn(1000);	      							
+					console.log('Email is INVALID');
+					event.preventDefault();
+				} 
+
+				//Проверка на правильность введения email адреса и пароля
+				if ( (emailVal==="mail@mail.com")&&(passwordVal==123) ){
+					$('#enterInstruction').fadeOut(1000);
+					console.log('Email and password is CORRECT');
+					_form.unbind("submit").submit();
+
+				} else {
+					$('#enterInstruction').fadeIn(1000);
+					console.log('Email and password is NOT CORRECT');
+					//event.preventDefault();
+					
+				}
+						
+			}
+				
+		};
+
+		// Возвращаем публичные медоты, которые будут доступны снаружи
+		return {
+			init
+		}
+	
+	}());
+  
+	// Запускаем модуль
+	myModule.init();
 
 });
--- js/test.js
+++ js/test.js
@@ -3,45 +3,53 @@ $(document).ready(function(){
 	var myModule = (function(){
 
 		// Переменные модуля
-		var _form = $('#comment-form');
-		var _commentTextarea = $('#commentText');
-		var _commentErrorEmpty = $('#commentErrorEmpty');
+		var _form = $('#form'),
+			 _yourEmail = $('input[type=email]'),
+			 _yourPassword = $('input:password'),
+			 _button = $('a.button'),
+			 isValid = true;
+		
 
 		// Метод инициализации (запуска) модуля
 		var init = function(){
 			_setUpListeners(); // Запускаем прослушку событий
-		}
-
-		// Метод прослушки событий
-		// В нем прослушиваем события на странице, например клики по кнопкам, и при срабатывании события запускаем нужный метод нашего модуля
-		var _setUpListeners = function(){
-			_form
-			.on('submit', function(event){_formValidation(event)});
-			_commentTextarea
-			.on('focus', function(event){_errorMessageClear(event)});
-
-		}
-
-		// Приватные методы
-    
-		var _formValidation = function (event) {
-    		//event.preventDefault();
-			console.log('Hello from _formValidation()');
-
-			if (_commentTextarea.val().trim().length==0){
-				_commentErrorEmpty.fadeIn(500);
-				event.preventDefault();
-				
-			} else {
-				_commentErrorEmpty.fadeOut(500);
-				//_form.unbind('submit').submit();
-			}
 		};
 
-		var _errorMessageClear = function (event) {
-			event.preventDefault();
-			_commentErrorEmpty.fadeOut(500);
+		// Метод прослушки событий 
+		// В нем прослушиваем события на странице, например клики по кнопкам, и при срабатывании события запускаем нужный метод нашего модуля
+		var _setUpListeners = function(){
+			_button.on('click', function(event){
+				_formValidation(event);
+			});
 		}
+
+		Приватные методы
+    
+		var _formValidation = function (event) {
+    		
+			console.log('Hello from _formValidation()');
+			var emailVal = _yourEmail.val().trim(),
+			 	passwordVal = _yourPassword.val().trim();
+			 	//inputs = _form.find('input');
+
+			 if (emailVal.length ==0){
+			 	$('#emailError').fadeIn(1000);
+			 	event.preventDefault();
+			 } else {
+			 	$('#emailError').fadeOut(1000);
+			 };
+
+			 if (passwordVal.length == 0){
+			 	$('#passwordError').fadeIn(1000);
+			 	event.preventDefault();
+			 } else {
+			 	$('#passwordError').fadeOut(1000);
+			 	
+			 });
+			 	
+
+				}
+			}
 
 		// Возвращаем публичные методы, которые будут доступны снаружи
 		return {
