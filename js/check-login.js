31c1a6b03307ecfb5d6d3d6f50d5a9facf178ad8
Andrey <lee1975uz@gmail.com>
2019-05-26 23:09:45 -0500

    ДЗ1.2.3 на рассмотрение
    1) В ДЗ1 исправил, и при фокусе все сообщение об ошибке удаляются.
    2) ДЗ2 и ДЗ3 представлены на рассмотрение.

--- js/check-login.js
+++ js/check-login.js
@@ -1,88 +1,113 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	var myModule = (function(){
-		// Переменные модуля
-		var _form = $('#form'),
-			_yourEmail = $('input[type=email]'),
-			_yourPassword = $('input:password');
-			
-			
-		// Метод инициализации (запуска) модуля
-		var init = function(){
-			_setUpListeners(); // Запускаем прослушку событий
-		};
+    var myModule = (function() {
+        // Переменные модуля
+        var _form = $('#form'),
+            _yourEmail = $('input[type=email]'),
+            _yourPassword = $('input:password');
 
-		
 
-		//Метод прослушки событий
-		//В нем прослушиваем события на странице, например клики по кнопкам, и при срабатывании события запускаем нужный метод нашего модуля
-		var _setUpListeners = function(){
-			_form.on('submit', function(event){
-				_formValidation(event);				
-			});
-			
-		}
+        // Метод инициализации (запуска) модуля
+        var init = function() {
+            _setUpListeners(); // Запускаем прослушку событий
+        };
 
-		// Приватные методы
-    
-		var _formValidation = function (event) {
-    		event.preventDefault();
-			console.log('Hello from _formValidation()');
-			var emailVal = _yourEmail.val().toLowerCase().trim(),
-			 	passwordVal = _yourPassword.val().trim();
-			 	
-			//Проверка пустые поля
-			 if (emailVal.length == 0){
-			 	$('#emailError').fadeIn(1000);
-			 	event.preventDefault();
-			 } else {
-			 	$('#emailError').fadeOut(1000);
-			 };
 
-			 if (passwordVal.length == 0){
-			 	$('#passwordError').fadeIn(1000);
-			 	event.preventDefault();
-			 } else {
-			 	$('#passwordError').fadeOut(1000);			 	
-			 }
 
-			 //Проверка на правильный формат email
-			 if ( emailVal !== '' ) {			 	
-				var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
-				if ( pattern.test( emailVal ) ) {
-					$('#wrongEmailFormat').fadeOut(1000);
-					console.log('Email is VALID');
-				} else {
-					$('#wrongEmailFormat').fadeIn(1000);	      							
-					console.log('Email is INVALID');
-					event.preventDefault();
-				} 
+        //Метод прослушки событий
+        //В нем прослушиваем события на странице, например клики по кнопкам, и при срабатывании события запускаем нужный метод нашего модуля
+        var _setUpListeners = function() {
+            _form.on('submit', function(event) {
+                _formValidation(event);
+            });
+            _yourEmail.on('focus', function(event) {
+                _clearEmailError(event);
+            });
+            _yourPassword.on('focus', function(event) {
+                _clearPasswordError(event);
+            });
 
-				//Проверка на правильность введения email адреса и пароля
-				if ( (emailVal==="mail@mail.com")&&(passwordVal==123) ){
-					$('#enterInstruction').fadeOut(1000);
-					console.log('Email and password is CORRECT');
-					_form.unbind("submit").submit();
+        }
 
-				} else {
-					$('#enterInstruction').fadeIn(1000);
-					console.log('Email and password is NOT CORRECT');
-					//event.preventDefault();
-					
-				}
-						
-			}
-				
-		};
+        // Приватные методы
 
-		// Возвращаем публичные медоты, которые будут доступны снаружи
-		return {
-			init
-		}
-	
-	}());
-  
-	// Запускаем модуль
-	myModule.init();
+        var _formValidation = function(event) {
+            event.preventDefault();
+            console.log('Hello from _formValidation()');
+            var emailVal = _yourEmail.val().toLowerCase().trim(),
+                passwordVal = _yourPassword.val().trim();
+
+            //Проверка пустые поля
+            if (emailVal.length == 0) {
+                $('#emailError').fadeIn(600);
+                event.preventDefault();
+            } else {
+                $('#emailError').fadeOut(600);
+            };
+
+            if (passwordVal.length == 0) {
+                $('#passwordError').fadeIn(600);
+                event.preventDefault();
+            } else {
+                $('#passwordError').fadeOut(600);
+            }
+
+            //Проверка на правильный формат email
+            if (emailVal !== '') {
+                var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+                if (pattern.test(emailVal)) {
+                    $('#wrongEmailFormat').fadeOut(600);
+                    var valid = true;
+                } else {
+                    $('#wrongEmailFormat').fadeIn(600);
+                    console.log('Email is INVALID');
+                    event.preventDefault();
+                    valid = false;
+                }
+
+                //Проверка на правильность введения email адреса и пароля
+                if (valid == true) {
+                    if ((emailVal === "mail@mail.com") && (passwordVal == 123)) {
+                        $('#enterInstruction').fadeOut(600);
+                        console.log('Email and password is CORRECT');
+                        _form.unbind("submit").submit();
+
+                    } else {
+                        $('#enterInstruction').fadeIn(600);
+                        console.log('Email and password is NOT CORRECT');
+                        event.preventDefault();
+
+                    }
+                }
+
+
+            }
+
+        };
+
+        // При фокусе сообщения об ошибке удаляются
+
+        var _clearEmailError = function(event) {
+            event.preventDefault();
+            $('#emailError').fadeOut(600);
+            $('#wrongEmailFormat').fadeOut(600);
+            $('#enterInstruction').fadeOut(600);
+        }
+
+        var _clearPasswordError = function(event) {
+            event.preventDefault();
+            $('#passwordError').fadeOut(600);
+            $('#enterInstruction').fadeOut(600);
+        }
+
+        // Возвращаем публичные медоты, которые будут доступны снаружи
+        return {
+            init
+        }
+
+    }());
+
+    // Запускаем модуль
+    myModule.init();
 
 });
--- js/check-registration.js
+++ js/check-registration.js
@@ -1,5 +1,129 @@
 $(document).ready(function() {
 
-	console.log('Hello world!');
+    var HomeWork3 = (function() {
+
+        // Переменные модуля
+        var _form = $('#form'),
+            _yourEmail = $('input[type=email]'),
+            _yourPassword = $('input:password');
+
+        // Метод инициализации (запуска) модуля
+        var init = function() {
+            _setUpListeners(); // Запускаем прослушку событий
+        }
+
+        // Метод прослушки событий
+        // В нем прослушиваем события на странице, например клики по кнопкам, и при срабатывании события запускаем нужный метод нашего модуля
+        var _setUpListeners = function() {
+            _form
+                .on('submit', function(event) {
+                    _formValidation(event);
+                });
+
+            _yourEmail.on('focus', function(event) {
+                _emailFocus(event)
+            }); //При фокусе удаляет все сообщения об ошибке
+
+            _yourPassword.on('focus', function(event) {
+                _passwordFocus(event)
+            }); //При фокусе удаляет все сообщения об ошибке
+        }
+
+
+        // Приватные методы
+
+        var _formValidation = function(event) {
+            event.preventDefault();
+            console.log('Hello from _actionOnClick()');
+            var inputs = _form.find('input.input-form-registration');
+
+            //Проверка пустые поля
+
+            $.each(inputs, function(index, val) {
+                var input = $(val),
+                    value = input.val().trim(),
+                    label = $(input).attr('placeholder').toLowerCase(),
+                    inputType = $(input).attr('type') + "Error",
+                    textError = 'Введите ' + label,
+                    tooltip = $('<span class="form__tooltip">' + textError + '</span>'),
+                    errorMessage = $('#' + inputType);
+
+
+                //Проверка полей на заполненность 
+
+                if (value.length == 0) {
+                    errorMessage.empty();
+                    errorMessage.fadeIn(700);
+                    errorMessage.addClass("notify notify--error mb-20");
+                    tooltip.appendTo(errorMessage);
+                    event.preventDefault();
+                } else {
+                    errorMessage.empty();
+                    errorMessage.fadeOut(700);
+                    errorMessage.removeClass("notify notify--error mb-20");
+                }
+
+
+                //Проверка на соответствие email формату
+
+                if (input.attr('type').toLowerCase() === 'email' && ($("input:password").val() != "")) {
+                    if (value !== '') {
+                        var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+                        if (pattern.test(value)) {
+                            $('#wrongEmailFormat').fadeOut(600);
+                            var valid = true;
+                        } else {
+                            $('#wrongEmailFormat').fadeOut(600);
+                            $('#wrongEmailFormat').fadeIn(600);
+                            console.log('Email is INVALID');
+                            event.preventDefault();
+                            console.log('FALSE FALSE FALSE');
+                            valid = false;
+                        }
+                    }
+                    console.log('VALIDVALID= ' + valid);
+                }
+
+                //Проверка на правильность введения email адреса и пароля
+
+                if (valid == true) {
+                    if (value === "mail@mail.com") {
+                        console.log('Email IS CORRECT');
+                        $('#emailInstruction').fadeOut(600);
+                        _form.unbind('submit').submit();
+
+                    } else {
+                        console.log('Email IS NOT CORRECT');
+                        event.preventDefault();
+                        $('#emailInstruction').fadeIn(600);
+                    }
+                }
+
+            })
+        };
+
+        // При фокусе удаляются сообщения об ошибке
+
+        var _emailFocus = function(event) {
+            event.preventDefault();
+            $("#emailError").fadeOut(600);
+            $('#emailInstruction').fadeOut(600);
+            $('#wrongEmailFormat').fadeOut(600);
+        }
+
+        var _passwordFocus = function(event) {
+            event.preventDefault();
+            $("#passwordError").fadeOut(600);
+        }
+
+        // Возвращаем публичные методы, которые будут доступны снаружи
+        return {
+            init
+        }
+
+    }());
+
+    // Запускаем модуль
+    HomeWork3.init();
 
 });
